c7e8468ad562b9a92cd67919d89ad77f9686cb0e
KJOHN <mayacafe123@gmail.com>
2017-07-22 04:05:34 +0800

    delete trash

--- hello/popup.js
+++ hello/popup.js
@@ -1,6 +1,6 @@
 var url = window.location.href;
 function go(url) {
-  window.open('localhost:9487/test.html', '_blank');
+  window.open('.html'+'&'+url, '_blank');
   return url;
 }
 document.getElementById("url").innerHTML = go(url);
